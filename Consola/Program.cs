98e388377b62de1a823551196eb1e054fbf072bb
jpinformaticaok <juanpabloaguero55@gmail.com>
2026-05-19 01:09:31 -0300

    Añadido nuevo proyecto de consola .NET 8.0
    Se ha agregado el proyecto **Consola** a la solución `InventarioISP20.sln`, configurado como una aplicación de consola con el framework de destino `net8.0`.
    
    El archivo `Consola.csproj` utiliza el SDK de .NET y habilita las características `ImplicitUsings` y `Nullable`.
    
    Se añadió el archivo `Program.cs` con la implementación del programa principal, que incluye:
    - Declaración de variables de ejemplo.
    - Impresión de parámetros de línea de comandos.
    - Captura del año de nacimiento del usuario y cálculo de su edad aproximada.

---
+++ Consola/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        CreamosVariables();
+        
+        Console.WriteLine("Probando imprimir algo en la pantalla");
+        ImpresionDeParametros(args);
+
+        CapturaDeValoresDelUsuario();
+    }
+
+    private static void CapturaDeValoresDelUsuario()
+    {
+        Console.Write("Ingrese el año de su nacimiento: ");
+        string? anio_nacimiento = Console.ReadLine();
+        int anio = Convert.ToInt32(anio_nacimiento);
+        int edad = DateTime.Now.Year - anio;
+        Console.WriteLine($"Su edad es aproximada es: {edad}");
+    }
+
+    private static void ImpresionDeParametros(string[] args)
+    {
+        if (args.Length > 1)
+            Console.WriteLine($"Hola: {args[0]} {args[1]}");
+    }
+
+    private static void CreamosVariables()
+    {
+        int numero = 10;
+        int numero2;
+
+        numero2 = 20;
+        int numero3 = 30;
+    }
+}
